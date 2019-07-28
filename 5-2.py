ca6714424a6047fc7c527e3018c7b1383922286a
adael-cohen <adael.cohen@gmail.com>
2019-07-28 19:16:09 +0300

    test

--- 5-2.py
+++ 5-2.py
@@ -22,6 +22,10 @@
 # print("The exchange will be: ", num_of_shekel / float(exchange), "US Dollar")
 # print("Thanks for using our currency converter")
 # 3434343434
+# swdsd
+# sdfsdfdsf
+
+
 
 import urllib.request, json
 def get_data_from_url(url):
